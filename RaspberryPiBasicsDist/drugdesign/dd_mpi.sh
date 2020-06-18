1d810d6f1675b09f090667036f0b53663234ee24
Dick Brown <rab@stolaf.edu>
2020-06-18 13:13:36 -0500

    remove RaspberryPiBasicsDist

--- RaspberryPiBasicsDist/drugdesign/dd_mpi.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-#SBATCH -o ./jobout/dd_mpi.out -e ./jobout/dd_mpi.err
-#SBATCH -n 4
-
-mpirun -x LD_LIBRARY_PATH=/usr/lib/openmpi/lib ./dd_mpi 
--- RaspberryPiBasicsDist/drugdesign/dd_mpi_cder.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-#SBATCH -o ./jobout/dd_mpi_cder.out -e ./jobout/dd_mpi_cder.err
-#SBATCH -n 4
-
-mpirun -x LD_LIBRARY_PATH=/usr/lib/openmpi/lib ./dd_mpi_cder 
